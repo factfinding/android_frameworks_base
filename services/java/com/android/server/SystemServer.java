1f54625dcf1746951b06eb67fb517dfd664f8b90
Erfan Abdi <erfangplus@gmail.com>
2026-07-08 17:31:45 +0800

    base: Switch to waydroid framework
    Co-authored-by: SupeChicken666 <me@supechicken666.dev>

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -339,6 +339,8 @@ import java.util.TreeSet;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.Future;
 
+import id.waydro.server.WaySystemServer;
+
 /**
  * Entry point to {@code system_server}.
  */
@@ -3124,6 +3126,10 @@ public final class SystemServer implements Dumpable {
                 | NoSuchMethodException e) {
             reportWtf("Making " + externalServer + " ready", e);
         }
+
+        t.traceBegin("startWaydroidServices");
+        WaySystemServer.startServices(context, mSystemServiceManager);
+        t.traceEnd();
 
         // It is now time to start up the app processes...
 
